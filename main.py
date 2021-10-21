c3de55500230e3829d8435b930705f6b0734fee1
blankalmasry <blankhussien@gmail.com>
2021-10-21 20:41:14 +0200

    moved most functions

--- main.py
+++ main.py
@@ -95,15 +95,6 @@ FUNCTIONS
 
 # TODO move functions to other files
 
-async def create_match_response(data):
-    msg = "**New Ratings**\n"
-    for user in data["users"]:
-        user_mention = discord.utils.find(lambda n: str(n) == user["name"], bot.get_all_members())
-        msg += f"{user_mention.mention}" + "\n" + "`Rank : " + user["rank"][
-            "rank"] + " " + str(round(user["rank"]["points"])) + " LP" + "\n" + "Rating : " + str(
-            round(user["rating"])) + "`\n"
-    return msg
-
 
 async def get_player_stats(player, guild_id):
     player_stats = await get_user_stats_request(guild_id, player)
@@ -115,71 +106,11 @@ async def get_player_stats(player, guild_id):
     return user_stats_message + user_rank_message
 
 
-async def fetch_user_who_got_mentions_or_message_author(message):
-    mentions = message.mentions
-    if len(mentions) > 1:  # a user got mentioned
-        player = mentions[1]
-    else:
-        player = message.author
-    return player
-
-
 async def create_match(winners, losers, guild_id):
     new_match = await match_factory(winners, losers)
     res = await create_match_request(guild_id, new_match)
     msg = await create_match_response(res)
     return msg
-
-
-async def start_voting(ctx):
-    message = await ctx.send('Which team won?')
-    await message.add_reaction("⬅")
-    await message.add_reaction("➡")
-    return message
-
-
-async def fetch_first_and_second_team(ctx):
-    mentions_len = len(ctx.message.mentions) - 1
-    team_1 = ctx.message.mentions[1: (mentions_len // 2) + 1]
-    team_2 = ctx.message.mentions[mentions_len // 2 + 1:]
-    first_team_players = tuple(map(lambda player: player.name + "#" + player.discriminator, team_1))
-    second_team_players = tuple(map(lambda player: player.name + "#" + player.discriminator, team_2))
-    return first_team_players, second_team_players
-
-
-async def force_admin_decision(reaction, first_team_players, second_team_players):
-    if str(reaction.emoji) == "⬅":
-        return first_team_players, second_team_players
-    if str(reaction.emoji) == "➡":
-        return second_team_players, first_team_players
-
-
-async def count_if_votes_efficient(left, right, first_team_players, second_team_players):
-    all_players = (first_team_players + second_team_players)
-    if len(left) >= 0.75 * len(all_players):
-        return True, first_team_players, second_team_players
-    elif len(right) >= 0.75 * len(all_players):
-        return True, second_team_players, first_team_players
-    else:
-        return False, None, None
-
-
-async def vote(voting_pool, first_team_players, second_team_players):
-    all_players = (first_team_players + second_team_players)
-    left = set()
-    right = set()
-    for reaction in voting_pool.reactions:
-        if reaction.emoji == "⬅":
-            async for user in reaction.users():
-                if user != bot.user and \
-                        user.name + "#" + user.discriminator in all_players:
-                    left.add(user)
-        if reaction.emoji == "➡":
-            async for user in reaction.users():
-                if user != bot.user and \
-                        user.name + "#" + user.discriminator in all_players:
-                    right.add(user)
-    return tuple(left), tuple(right)
 
 
 async def get_leaderboard(guild_id, author, page=1):
@@ -195,6 +126,29 @@ async def get_leaderboard(guild_id, author, page=1):
 
 def is_bot(user):
     return user == bot.user
+
+
+async def create_match_response(data):
+    msg = "**New Ratings**\n"
+    for user in data["users"]:
+        user_mention = await find_user(user["name"])
+        msg += f"{user_mention.mention}" + "\n" + "`Rank : " + user["rank"][
+            "rank"] + " " + str(round(user["rank"]["points"])) + " LP" + "\n" + "Rating : " + str(
+            round(user["rating"])) + "`\n"
+    return msg
+
+
+async def find_user(name):
+    return discord.utils.find(lambda n: str(n) == name, bot.get_all_members())
+
+
+async def fetch_user_who_got_mentions_or_message_author(message):
+    mentions = message.mentions
+    if len(mentions) > 1:  # a user got mentioned
+        player = mentions[1]
+    else:
+        player = message.author
+    return player
 
 
 bot.run(getenv("DISCORD_API"))
--- match.py
+++ match.py
@@ -4,6 +4,7 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
+
 load_dotenv()
 
 intents = discord.Intents.default()
@@ -37,3 +38,56 @@ def validate_teams(message):
     # Lazy validation, would fix an early `vs` or a late one by auto-balancing teams
     if (len(message.mentions) - 1) % 2 != 0:
         return "uneven teams"
+
+
+async def count_if_votes_efficient(left, right, first_team_players, second_team_players):
+    all_players = (first_team_players + second_team_players)
+    if len(left) >= 0.75 * len(all_players):
+        return True, first_team_players, second_team_players
+    elif len(right) >= 0.75 * len(all_players):
+        return True, second_team_players, first_team_players
+    else:
+        return False, None, None
+
+
+async def vote(voting_pool, first_team_players, second_team_players):
+    all_players = (first_team_players + second_team_players)
+    left = set()
+    right = set()
+    for reaction in voting_pool.reactions:
+        if reaction.emoji == "⬅":
+            async for user in reaction.users():
+                if user != bot.user and \
+                        user.name + "#" + user.discriminator in all_players:
+                    left.add(user)
+        if reaction.emoji == "➡":
+            async for user in reaction.users():
+                if user != bot.user and \
+                        user.name + "#" + user.discriminator in all_players:
+                    right.add(user)
+    return tuple(left), tuple(right)
+
+
+async def force_admin_decision(reaction, first_team_players, second_team_players):
+    if str(reaction.emoji) == "⬅":
+        return first_team_players, second_team_players
+    if str(reaction.emoji) == "➡":
+        return second_team_players, first_team_players
+
+
+async def fetch_first_and_second_team(ctx):
+    mentions_len = len(ctx.message.mentions) - 1
+    team_1 = ctx.message.mentions[1: (mentions_len // 2) + 1]
+    team_2 = ctx.message.mentions[mentions_len // 2 + 1:]
+    first_team_players = tuple(map(lambda player: player.name + "#" + player.discriminator, team_1))
+    second_team_players = tuple(map(lambda player: player.name + "#" + player.discriminator, team_2))
+    return first_team_players, second_team_players
+
+
+async def start_voting(ctx):
+    message = await ctx.send('Which team won?')
+    await message.add_reaction("⬅")
+    await message.add_reaction("➡")
+    return message
+
+
