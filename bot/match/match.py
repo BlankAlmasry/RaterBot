5f529ba0c31f9088e1474e5f7a300bce16693333
blankalmasry <blankhussien@gmail.com>
2021-10-23 13:53:12 +0200

    match_factore return namedtuple and get convered when needed

--- bot/match/match.py
+++ bot/match/match.py
@@ -28,18 +28,18 @@ async def execute_result(ctx, voting_pool, losers, winners):
 
 async def create_match(winners, losers, guild_id, users):
     match = await match_factory(winners, losers)
-    res = await create_match_request(guild_id, match)
+    res = await create_match_request(guild_id, match._asdict())
     match_response = await create_match_response(res, users)
     return match_response
 
 
-async def match_factory(winners, losers):
+async def match_factory(winners, losers) -> namedtuple:
     Match = namedtuple("Match", "teams")
     match_data = Match([
         {"users": winners, "result": 1},
         {"users": losers, "result": 0}
     ])
-    return match_data._asdict()
+    return match_data
 
 
 async def match_voting_pool_handler(message, first_team_players, second_team_players,
